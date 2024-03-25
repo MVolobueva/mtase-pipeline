553bf3660aed15726c387a6ffb7530fcc5365246
Maria Samokhina <72346144+MVolobueva@users.noreply.github.com>
2024-03-25 17:28:14 +0000

    MTase

--- pages/1_MTase_detection_and_classification.py
+++ pages/1_MTase_detection_and_classification.py
@@ -207,6 +207,9 @@ if uploaded_file is not None:
     #st.write(os.system('chmod 777 ./pipelineFiles/classification.py'))
     main()
     st.write('## Step 3 output - classified MTases')
-    st.dataframe(pd.read_csv('./pipelineFiles/class.tsv', sep='\t', index_col = 0))
-    st.write('## Step 3 output - MTases with several catalytic domains')
-    st.dataframe(pd.read_csv('./pipelineFiles/several_cat_domains.tsv', sep='\t', index_col = 0))
+    if pd.read_csv('./pipelineFiles/class.tsv', sep='\t', index_col = 0):
+        st.dataframe(pd.read_csv('./pipelineFiles/class.tsv', sep='\t', index_col = 0))
+        st.write('## Step 3 output - MTases with several catalytic domains')
+        st.dataframe(pd.read_csv('./pipelineFiles/several_cat_domains.tsv', sep='\t', index_col = 0))
+    else:
+        st.write('No catalytic domain were found')
