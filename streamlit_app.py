0a3c411e7ae1d46dcf76e87f5cfb058163b523c2
Maria Samokhina <72346144+MVolobueva@users.noreply.github.com>
2024-03-25 13:05:41 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,9 @@ st.sidebar.title("Pipeline steps")
 st.sidebar.write('## Step 1')
 uploaded_file = st.sidebar.file_uploader("Load sequences in fasta format")
 if uploaded_file is not None:
-    with open(os.path.join("./TempDir",uploaded_file.name),"wb") as f:
+    with open(os.path.join(".",uploaded_file.name),"wb") as f:
          f.write(uploaded_file.getbuffer())
-    print(os.path.join("./TempDir",uploaded_file.name))                                
+    print(os.path.join(".",uploaded_file.name))                                
     os.system('hmmsearch --cpu 3 -E 0.01 --domE 0.01 --incE 0.01 --incdomE 0.01 \
         -o /dev/null --noali -A ./pipelineFiles/file.stk\
         ./pipelineFiles/selected_profiles.hmm ' + os.path.join("TempDir",uploaded_file.name))
