fdc50c161a3f990ad28c6c346f4324da05a77403
Maria Samokhina <72346144+MVolobueva@users.noreply.github.com>
2024-03-25 14:18:48 +0000

    ss

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,7 @@ if uploaded_file is not None:
     st.sidebar.write('Step 2 finished')
     st.sidebar.write('## Step 3')
     #st.write(os.system('chmod 777 ./pipelineFiles/classification.py'))
-    try : 
-      st.write(os.system('./pipelineFiles/classification.py --t ./pipelineFiles/region_alignments.tsv --m ./pipelineFiles/several_cat_domains.tsv --c ./pipelineFiles/class.tsv'))  
-     
-    except OSError as error : 
-      st.write(error)
-      print("File descriptor is not associated with any terminal device") 
-    #st.write(os.system('./pipelineFiles/classification.py --t ./pipelineFiles/region_alignments.tsv --m ./pipelineFiles/several_cat_domains.tsv --c ./pipelineFiles/class.tsv'))
+    st.write(os.system('./classification.py --t ./pipelineFiles/region_alignments.tsv --m ./pipelineFiles/several_cat_domains.tsv --c ./pipelineFiles/class.tsv'))
 
     st.dataframe(pd.read_csv('./pipelineFiles/class.tsv', sep='\t'))
 
