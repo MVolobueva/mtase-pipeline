81ca1098ed3909ee39cbae3b616096106a665aa9
Maria Samokhina <72346144+MVolobueva@users.noreply.github.com>
2024-03-28 14:43:53 +0000

    dd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="DNA-MTases"
-)
-from st_pages import Page, show_pages, add_page_title
+#st.set_page_config(
+#    page_title="DNA-MTases"
+#)
+#from st_pages import Page, show_pages, add_page_title
 
 # Optional -- adds the title and icon to the current page
 #add_page_title()
